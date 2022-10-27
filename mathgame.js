a02c8656935a1895775cd7076c69f54780a26ac7
ZioXerXes <88509035+ZioXerXes@users.noreply.github.com>
2022-10-27 17:58:46 -0500

    Working on restarting problem after success

--- mathgame.js
+++ mathgame.js
@@ -1,20 +1,31 @@
-var a = Math.floor(Math.random()*10)
-var b = Math.floor(Math.random()*10)
-var answer = a+b;
-
-var questionGen = function () {  
-  return a + '+' + b
-}
-
 $(document).ready(function(){
-  $('.question').append('<p>' + questionGen() + '</p>');
-  console.log(a)
-  console.log(b)
-  console.log(answer)
+  
+  var questionGen = function () {  
+    var a = Math.floor(Math.random()*10)
+    var b = Math.floor(Math.random()*10)
+    return a + '+' + b
+  }
+
+
+  var squisher = eval($('.question').append('<p>' + questionGen() + '</p>').children('p').html())
+  console.log(squisher);
 
   $('#answer').on('submit', function (event) {
     event.preventDefault();
-    var cleanAnswer = parseInt($(this).children('[name=userAnswer]').val());
-    console.log(cleanAnswer);
+    inputSelector = $(this).children('[name=userAnswer]');
+    var cleanAnswer = parseInt(inputSelector.val());
+    
+    if (cleanAnswer == squisher) {
+      console.log('You did it!');
+      $('.fate').empty();
+      $('.fate').append('<p>You did it!</p>');
+      inputSelector.val('');
+      $('.question').empty()
+      return questionGen()
+    } else {
+      console.log('You suck!');
+      $('.fate').empty();
+      $('.fate').append('<p>You suck!</p>');
+    }
   })
 });
