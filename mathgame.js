42f314c1aa68f2f5781c2d1326dd1ec5ce63824c
ZioXerXes <88509035+ZioXerXes@users.noreply.github.com>
2022-10-27 16:49:45 -0500

    Answer variable, generators made global

--- mathgame.js
+++ mathgame.js
@@ -1,7 +1,14 @@
-var questionGen = function () {
-  return Math.floor(Math.random()*10) + '+' + Math.floor(Math.random()*10)
+var a = Math.floor(Math.random()*10)
+var b = Math.floor(Math.random()*10)
+var answer = a+b;
+
+var questionGen = function () {  
+  return a + '+' + b
 }
 
 $(document).ready(function(){
   $('.question').append('<p>' + questionGen() + '</p>');
+  console.log(a)
+  console.log(b)
+  console.log(answer)
 });
