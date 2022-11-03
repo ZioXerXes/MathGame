6cd7b92f2acf6069aa89055f0360c97214d88498
ZioXerXes <88509035+ZioXerXes@users.noreply.github.com>
2022-11-02 19:54:07 -0500

    Timer stops at zero and gets added to with correct answer. About to chang from milliseconds to seconds

--- mathgame.js
+++ mathgame.js
@@ -16,15 +16,38 @@ var newProblem = function () {
   return squisher;
 };
 
+
 $(document).ready(function(){
+
+  var timerSpan = document.body.querySelector("#timer");
+  var milliseconds = 1000;
+  var timer = null;
+  
+  var stopTimer = function () {
+    window.clearInterval(timer);
+    timer = null;
+    milliseconds = 1000;
+  };
+
+  var startTimer = function () {
+    if (!timer) {
+      timer = setInterval(function () {
+        timerSpan.innerHTML = --milliseconds;
+        if (milliseconds === 0) {
+          console.log('WOAH!')
+          //document.getElementById('timer').innerHTML = '<span>0</span>';
+          stopTimer();
+        }
+      }, 1);
+      }
+    };
+  
 
   $('#starter').on('click', function (event) {
     console.log("Starter triggered");
     event.preventDefault();
-    newProblem()
-    //var answerHole = newProblem();
-    //console.log(answerHole + 'meow' + answerHole)
-
+    startTimer();
+    newProblem();
   })
 
   $('#answer').on('submit', function (event) {
@@ -33,17 +56,18 @@ $(document).ready(function(){
     inputSelector = $(this).children('[name=userAnswer]');
     var cleanAnswer = parseInt(inputSelector.val());
     console.log(cleanAnswer)
-    
+
     $.answerHole = eval($('.question').children('p').html())
 
     console.log('answerHole = ' + $.answerHole)
     if (cleanAnswer == $.answerHole) {
+      console.log(milliseconds.charAt[0])
       console.log('You did it!');
       $('.fate').empty();
       $('.fate').append('<p>You did it!</p>');
+      milliseconds += 100;
       inputSelector.val('')
       $('.question').empty();
-      //$('.question').append('<p>' + questionGen() + '</p>').children('p')
       return newProblem()
     } else {
       console.log('You suck!');
